49cd00199b4a2680e27dcf95faa9aee47e2edd8b
BeloneDF <belonefraga1@hotmail.com>
2024-07-16 15:02:24 -0300

    Bug celular

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ import { verify } from "jsonwebtoken";
 
 function runServer() {
   const app = new Elysia();
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
   app.use(AuthRoutes());
   app.guard(
     {
