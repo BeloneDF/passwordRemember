ed6718b5bb65e48a86bf0d5560f2cf44fa77ecd1
BeloneDF <belonefraga1@hotmail.com>
2024-09-03 16:43:06 -0300

    senhas adicionadas recentementes

--- src/controllers/passwords/password.ts
+++ src/controllers/passwords/password.ts
@@ -45,6 +45,9 @@ export async function GetPasswordsByUserId(id: string) {
       where: {
         userId: id,
       },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
     const token = jwt.sign(
       {
